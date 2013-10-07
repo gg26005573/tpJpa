a0cb401116f3645ec44bb75c3e4144eade242b15
Guillaume Gauvrit <26005573@e103m10.istic.univ-rennes1.fr>
2013-10-07 18:17:56 +0200

    tpJpa OK

---
+++ src/main/java/fr/istic/tpjpa/jpa/Device.java
@@ -0,0 +1,46 @@
+package fr.istic.tpjpa.jpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+public class Device {
+	private Long id;
+	
+	private String name;
+	
+	public Device() {
+	}
+	
+	public Device(String n){
+		name = n;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+
+}
