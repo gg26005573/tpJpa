84b56e88b632dc547e8d71177af700cbad216e11
gg26005573 <guillaume.etu@caerbann.org>
2013-11-19 13:54:53 +0100

    selection menu added

--- src/main/java/fr/istic/tpjpa/client/TpJpaGWTEntryPoint.java
+++ src/main/java/fr/istic/tpjpa/client/TpJpaGWTEntryPoint.java
@@ -9,8 +9,10 @@ import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
+import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 
@@ -25,12 +27,50 @@ public class TpJpaGWTEntryPoint implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		
+		// Make a command that we will execute from all leaves.
+	    Command cmd = new Command() {
+	      public void execute() {
+	        Window.alert("You selected a menu item!");
+	      }
+	    };
+	    
+	    Command searchPersonCmd = new Command() {
+	      public void execute() {
+	    	  displayPersonSearch();
+	      }
+	    };
 
+	    // Make some sub-menus that we will cascade from the top menu.
+	    MenuBar searchMenu = new MenuBar(true);
+	    searchMenu.addItem("Person", searchPersonCmd);
+	    searchMenu.addItem("Home", cmd);
+
+	    MenuBar updateMenu = new MenuBar(true);
+	    updateMenu.addItem("Person", cmd);
+	    updateMenu.addItem("Home", cmd);
+
+
+	    // Make a new menu bar, adding a few cascading menus to it.
+	    MenuBar menu = new MenuBar();
+	    menu.addItem("Search", searchMenu);
+	    menu.addItem("Update", updateMenu);
+
+	    // Add it to the root panel.
+	    RootPanel.get("gwtMenu").add(menu);
+
+	}
+	
+	public void displayPersonSearch(){
+		RootPanel panel = RootPanel.get("gwtProcess");
+		
+		panel.clear();
 		// Create a text
 		final TextBox area = new TextBox();
 		area.setValue("1");
-		RootPanel.get("mainGwtEntryPoint").add(area);
-
+		panel.add(area);
+		
+		
 		// Create a button
 		com.google.gwt.user.client.ui.Button b = new Button();
 		b.setText("sudo display this person, now!");
@@ -52,13 +92,14 @@ public class TpJpaGWTEntryPoint implements EntryPoint {
 									.getInstance();
 							PersonItf p = converter
 									.deserializeFromJson(response.getText());
-//							 Window.alert("Name: " + p.getName()
-//							 + ", Devices: " + p.getDevices());
+//									 Window.alert("Name: " + p.getName()
+//									 + ", Devices: " + p.getDevices());
 							 Formater.displayPerson(
-									 RootPanel.get("displayPerson"),
+									 RootPanel.get("gwtResult"),
 									 p);
 							// Window.alert("Serialized person: " +
 							// response.getText());
+							 displayMario();
 						}
 					}
 				});
@@ -71,7 +112,10 @@ public class TpJpaGWTEntryPoint implements EntryPoint {
 			}
 		});
 
-		RootPanel.get("mainGwtEntryPoint").add(b);
-
+		panel.add(b);
+	}
+	
+	public void displayMario(){
+		RootPanel.get("mario").setStylePrimaryName("visible center");
 	}
 }
